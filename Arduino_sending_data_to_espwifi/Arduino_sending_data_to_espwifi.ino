b1d1aac1cb14e5b1a88a801d3d0d40c644a37212
junpeng gai <34943011+jeo33@users.noreply.github.com>
2023-07-01 02:47:38 -0400

    Add files via upload
    upload arduino code for communication between uno and espwifi module

---
+++ Arduino_sending_data_to_espwifi/Arduino_sending_data_to_espwifi.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+
+// SoftwareSerial object to communicate with ESP8266
+SoftwareSerial mySerial(2, 3); // Arduino RX pin 2 (ESP8266 TX), Arduino TX pin 3 (ESP8266 RX)
+
+void setup() {
+  // Start the serial communication
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  
+  // Wait for the ESP8266 to initialize
+  delay(1000);
+}
+int n=0;
+void loop() {
+ boolean ready=false;
+  n=analogRead(0);
+ mySerial.println(n);
+ 
+  delay(1000);
+}
---
+++ esp01_receiving_data_from_serial/esp01_receiving_data_from_serial.ino
@@ -0,0 +1,66 @@
+//
+// Copyright 2015 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// FirebaseDemo_ESP8266 is a sample that demo the different functions
+// of the FirebaseArduino API.
+
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+String sensorValue;
+#define FIREBASE_HOST "test-f13b1-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "yF8p3LnqpyHxBxQPS67UEV71NfYplrjcPBEDvdSp"
+#define WIFI_SSID "ggg"
+#define WIFI_PASSWORD "gggggggg"
+
+void setup() {
+  Serial.begin(9600);
+
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+int n = 0;
+int num ;
+void loop() {
+if (Serial.available()) {
+    // Read the incoming data
+    String line = Serial.readStringUntil('\n');
+       // set string value
+  Firebase.setString("message", line);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /message failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(1000);
+  }
+
+
+   
+ 
+}
